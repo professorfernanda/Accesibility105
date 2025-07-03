1a211582ee4e502f337ac9104851c976b787a96a
professorfernanda <fmurillo@sdgku.edu>
2025-07-03 13:36:18 -0700

    Initial Commit

---
+++ Accesibility105/Accesibility105App.swift
@@ -0,0 +1,17 @@
+//
+//  Accesibility105App.swift
+//  Accesibility105
+//
+//  Created by Fernanda Murillo on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Accesibility105App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accesibility105/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Accesibility105
+//
+//  Created by Fernanda Murillo on 03/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(systemName: "star.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .accessibilityLabel("This is my shape. Is a star.")
+                .accessibilityHint("Decorative Icon")
+            
+            Button(action: {
+                print("Button")
+            }) {
+                Text("Press")
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .accessibilityLabel("Button for Call to Action")
+            .accessibilityHint("Press the Button to Call an Action")
+            
+          
+            Text("This is a text")
+                .font(.title)
+                .accessibilityAddTraits(.isHeader)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
